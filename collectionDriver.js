5e6eac7b1173cd62e8fb3ce21cbbfd49177ab797
unknown <dafi.121@gmail.com>
2014-08-14 14:18:23 +0300

    first commit

---
+++ collectionDriver.js
@@ -0,0 +1,118 @@
+var ObjectID = require('mongodb').ObjectID;
+
+CollectionDriver = function(db) {
+  this.db = db;
+};
+
+CollectionDriver.prototype.getCollection = function(collectionName, callback) {
+  this.db.collection(collectionName, function(error, the_collection) {
+    if( error ) callback(error);
+    else callback(null, the_collection);
+  });
+};
+
+//find all objects for a collection
+CollectionDriver.prototype.findAll = function(collectionName, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+        the_collection.find().toArray(function(error, results) { //B
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+//find a specific object
+CollectionDriver.prototype.get = function(collectionName, id, callback) { //A
+    this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback(error)
+        else {
+            var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$"); //B
+            if (!checkForHexRegExp.test(id)) callback({error: "invalid id"});
+            else the_collection.findOne({'_id':ObjectID(id)}, function(error,doc) { //C
+            	if (error) callback(error)
+            	else callback(null, doc);
+            });
+        }
+    });
+}
+
+//find a user
+CollectionDriver.prototype.getUser = function (collectionName, userName,password,callback) {
+	this.getCollection (collectionName , function (error, the_collection) {
+	    if (error) callback(error)
+        else {
+              the_collection.findOne({'name':userName , 'password':password}, function(error,doc) { //C
+              if (error) callback(error)
+               else callback(null, doc);
+            });
+        }
+    });
+}
+
+
+
+
+//save new object
+CollectionDriver.prototype.save = function(collectionName, obj, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+      if( error ) callback(error)
+      else {
+        obj.created_at = new Date(); //B
+        the_collection.insert(obj, function() { //C
+          callback(null, obj);
+        });
+      }
+    });
+};
+
+//update a specific object
+CollectionDriver.prototype.update = function(collectionName, obj, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback(error)
+        else {
+	        obj._id = ObjectID(entityId); //A convert to a real obj id
+	        obj.updated_at = new Date(); //B
+                the_collection.save(obj, function(error,doc) { //C
+            	if (error) callback(error)
+            	else callback(null, obj);
+            });
+        }
+    });
+}
+
+//update location
+
+CollectionDriver.prototype.updateLocation = function(collectionName,obj, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) {
+        if (error) callback(error)
+        else {
+		console.log(obj);
+		the_collection.update({'_id':ObjectID(entityId)},
+					{ $set : {'curent_location':obj} },
+					function(error,doc) { //C
+            				    if (error) callback(error)
+            				    else callback(null, obj);
+                });
+
+        }
+    });
+}
+
+
+//delete a specific object
+CollectionDriver.prototype.delete = function(collectionName, entityId, callback) {
+    this.getCollection(collectionName, function(error, the_collection) { //A
+        if (error) callback(error)
+        else {
+            the_collection.remove({'_id':ObjectID(entityId)}, function(error,doc) { //B
+            	if (error) callback(error)
+            	else callback(null, doc);
+            });
+        }
+    });
+}
+
+exports.CollectionDriver = CollectionDriver;
